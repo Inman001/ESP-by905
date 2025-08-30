eedf770395898b7ba02ac69abcb48d70386e7fa0
Inman001 <ccujxhjyuot@gmail.com>
2025-08-30 21:22:12 +0700

    Delete esp.lua

--- esp.lua
+++
@@ -1,9 +0,0 @@
--- ESP Script (Highlight Enemies)
-for _, player in pairs(game.Players:GetPlayers()) do
-    if player.Character and player ~= game.Players.LocalPlayer then
-        local highlight = Instance.new("Highlight")
-        highlight.Parent = player.Character
-        highlight.FillColor = Color3.fromRGB(255, 0, 0) -- สีแดง
-        highlight.OutlineColor = Color3.fromRGB(255, 255, 255) -- เส้นขาว
-    end
-end
